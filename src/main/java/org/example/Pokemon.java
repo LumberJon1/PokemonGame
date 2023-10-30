88deb88ba5a9b5489cd591d620b76ac63405917f
Jonathan W <jwilliams12117@gmail.com>
2023-10-29 22:32:30 -0700

    Created some starting instance variables and skeleton of constructor, methods

--- src/main/java/org/example/Pokemon.java
+++ src/main/java/org/example/Pokemon.java
@@ -5,8 +5,33 @@ package org.example;
 public class Pokemon {
 
 //    Instance variables for the super class Pokemon
+    private String species = "Eevee";
+    private String name = "Eevee01";
+    private int hp = 10;
+    private int level = 1;
+    private int xp = 0;
+    private int speed = 10;
+    private int attack = 10;
+    private int defense = 10;
+    private int attackSpecial = 10;
+    private int defenseSpecial = 10;
+
+    private String type1 = "Normal";
+    private String type2 = "";
+
+//    TODO list for instance variables:
+    /*
+    * - Have a flag that will toggle a boolean for dual-typing
+    * - Have an ArrayList of types against which the pokemon is super effective or is half-effective
+    * - Have an items ArrayList
+    * */
+
 
 //    Constructors
+    public Pokemon(String species, String name) {
+
+
+    }
 
 //    Methods
 
