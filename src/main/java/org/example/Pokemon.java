7906bcbfa6d1c51899e6348584d386f9b5f60ab9
Jonathan W <jwilliams12117@gmail.com>
2023-10-29 22:17:45 -0700

    Initial commit of pokemon game main Pokemon class skeleton

---
+++ src/main/java/org/example/Pokemon.java
@@ -0,0 +1,18 @@
+package org.example;
+
+// Resolve any imports that I might need from utils etc.
+
+public class Pokemon {
+
+//    Instance variables for the super class Pokemon
+
+//    Constructors
+
+//    Methods
+
+    public static void main(String[] args) {
+
+        // Testbed for pokemon methods
+
+    }
+}
